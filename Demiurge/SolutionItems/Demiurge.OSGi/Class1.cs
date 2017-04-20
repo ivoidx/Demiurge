5bd512f955de964e64b0201ebaae6a6cde3e056b
ivoidx <ivoidx@163.com>
2017-04-20 11:48:38 +0800

    add

---
+++ Demiurge/SolutionItems/Demiurge.OSGi/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demiurge.OSGi
+{
+    public class Class1
+    {
+    }
+}
